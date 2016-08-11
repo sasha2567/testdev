638c75bdf06db20af37d0952d65ac42adbad3e2e
sasha2567 <sasha25678@mail.ru>
2016-08-11 18:38:47 +0300

    nginx routing

--- config/db.php
+++ config/db.php
@@ -1,5 +1,10 @@
 <?php
 
+if (empty($_ENV['DB_HOST'])) $_ENV['DB_HOST'] = 'localhost';
+if (empty($_ENV['DB_NAME'])) $_ENV['DB_NAME'] = 'test_db';
+if (empty($_ENV['DB_USER'])) $_ENV['DB_USER'] = 'postgres';
+if (empty($_ENV['DB_PASS'])) $_ENV['DB_PASS'] = 'postgres';
+
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'pgsql:host='.$_ENV['DB_HOST'].';port=5432;dbname='.$_ENV['DB_NAME'],
